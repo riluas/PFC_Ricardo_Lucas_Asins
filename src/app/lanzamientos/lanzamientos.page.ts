cbbd11a9f1ffc738c7c87bbf016223dec681560d
riluas <cutsay48@gmail.com>
2021-05-20 02:44:01 +0200

    Subida versión

--- src/app/lanzamientos/lanzamientos.page.ts
+++ src/app/lanzamientos/lanzamientos.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { disableDebugTools } from '@angular/platform-browser';
 import { HTTP } from '@ionic-native/http/ngx';
 import { SpaceServiceService } from '../space-service.service';
 @Component({
@@ -12,6 +13,8 @@ export class LanzamientosPage implements OnInit {
   infoLaunches: any;
   infoRocket: any;
   provSer: SpaceServiceService;
+  dateArray= [];
+  datarray: any;
 
   constructor(private http: HTTP, public spaceService: SpaceServiceService) {
     this.provSer = spaceService
@@ -20,11 +23,18 @@ export class LanzamientosPage implements OnInit {
   ngOnInit() {
     
     this.http.get('https://api.spacexdata.com/v4/launches/upcoming', {}, {})
-
     .then(res => {
       console.log(res + "HOLA");
       this.infoLaunches= JSON.parse(res.data);
       console.log(res.data); // Información recibida desde el server.
+      for (let i = 0; i < this.infoLaunches.length; i++) {
+        this.dateArray.push({"id":this.infoLaunches[i].id, "date":this.infoLaunches[i].date_utc})
+        console.log(this.dateArray);
+        
+        // this.datarray.push(this.infoLaunches[i].date_utc);
+      }
+ 
+      
     })
     .catch(error => {
       console.log(error.error); // Mensaje de error en una cadena.
@@ -40,12 +50,19 @@ export class LanzamientosPage implements OnInit {
     .catch(error => {
       console.log(error.error); // Mensaje de error en una cadena.
     });
+
+ 
     
   }
   conutDownDate = new Date("2021-05-26T18:59:00.000Z").getTime();
   setLaunchId(id){
     this.provSer.setLaunchId(id);
   }
+
+  yourMethod(id){
+    this.datarray.push(id);
+  }
+  
 
   demo:any
   x = setInterval(()=>{
