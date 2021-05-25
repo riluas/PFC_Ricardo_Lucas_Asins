06ebbd882f91762513ebbf8e4a01261ff3c7e284
riluas <cutsay48@gmail.com>
2021-05-25 10:12:04 +0200

    Añadido contenido a la pagina de detalles

--- src/app/lanzamiento-detalle/lanzamiento-detalle.page.ts
+++ src/app/lanzamiento-detalle/lanzamiento-detalle.page.ts
@@ -9,6 +9,7 @@ import { IFavorito } from '../interfaces';
   styleUrls: ['./lanzamiento-detalle.page.scss'],
 })
 export class LanzamientoDetallePage implements OnInit {
+  rocketName: any;
   meGustaIcon = false;
   infoLaunches: any;
   infoRocket: any;
@@ -16,113 +17,138 @@ export class LanzamientoDetallePage implements OnInit {
   provSer: SpaceServiceService;
   UsuarioIniciado: any;
   favoritos: IFavorito[] = [];
-  arrayFavoritos:any[] = Array();
-  arrayUsuariosFavoritos:any[] = Array();
+  arrayFavoritos: any[] = Array();
+  arrayUsuariosFavoritos: any[] = Array();
   idUsuarioIniciado: number;
   existe = false;
-  constructor(private http: HTTP, public spaceService: SpaceServiceService,private _db: AngularFireDatabase) {
+  rocketImage: any;
+  infoLaunchpad: any;
+  constructor(private http: HTTP, public spaceService: SpaceServiceService, private _db: AngularFireDatabase) {
     this.provSer = spaceService
-   }
+  }
 
   ngOnInit() {
-    
+
     this.UsuarioIniciado = this.provSer.getUser(); //recojo el usuario uniciado
     this.idLaunch = this.provSer.getLaunchId(); //recojo la id de lanzamiento la cual se ha pulsado en "me gusta"
     this.http.get('https://api.spacexdata.com/v4/launches/upcoming', {}, {})
 
-    .then(res => {
-      console.log(res + "HOLA");
-      this.infoLaunches= JSON.parse(res.data);
-      console.log(res.data); // Información recibida desde el server.
-    })
-    .catch(error => {
-      console.log(error.error); // Mensaje de error en una cadena.
-    });
+      .then(res => {
+        console.log(res + "HOLA");
+        this.infoLaunches = JSON.parse(res.data);
+        console.log(res.data); // Información recibida desde el server.
+      })
+      .catch(error => {
+        console.log(error.error); // Mensaje de error en una cadena.
+      });
 
     this.http.get('https://api.spacexdata.com/v4/rockets', {}, {})
 
+      .then(res => {
+        console.log(res + "HOLA");
+        this.infoRocket = JSON.parse(res.data);
+        console.log(res.data); // Información recibida desde el server.
+      })
+      .catch(error => {
+        console.log(error.error); // Mensaje de error en una cadena.
+      });
+
+    let ref = this.provSer.getFavoritos();
+    ref.once("value", snapshot => {
+      snapshot.forEach(child => {
+        let value = child.val();
+        this.favoritos.push(value);
+        console.log(this.favoritos);
+        console.log("He encontrado " + child.val().idLanzamiento);
+      })
+
+    });
+    
+    this.http.get('https://api.spacexdata.com/v4/launchpads', {}, {})
+
     .then(res => {
       console.log(res + "HOLA");
-      this.infoRocket= JSON.parse(res.data);
+      this.infoLaunchpad = JSON.parse(res.data);
       console.log(res.data); // Información recibida desde el server.
     })
     .catch(error => {
       console.log(error.error); // Mensaje de error en una cadena.
-    });
-
-    let ref = this.provSer.getFavoritos();
-    ref.once("value", snapshot =>{
-      snapshot.forEach(child =>{
-        let value = child.val();
-        this.favoritos.push(value);
-        console.log(this.favoritos);
-        console.log("He encontrado "+child.val().idLanzamiento);
-      })
-         
     });
 
     let ref2 = this.provSer.getFavoritos();
     ref2.orderByChild("usuario").equalTo(this.UsuarioIniciado).once("value", snapshot => {
-    snapshot.forEach(child => {
-      console.log(child.key+": "+child.val());
-      console.log(child.val().idLanzamiento);
-      if (child.val().idLanzamiento == this.idLaunch && child.val().usuario == this.UsuarioIniciado) {
+      snapshot.forEach(child => {
+        console.log(child.key + ": " + child.val());
+        console.log(child.val().idLanzamiento);
+        if (child.val().idLanzamiento == this.idLaunch && child.val().usuario == this.UsuarioIniciado) {
           this.existe = true;
           this.meGustaIcon = true;
-      }
-    })
+        }
+      })
     });
 
   }
-  
 
-  setLaunchId(idLanzamiento){
+  getInfo(id) {
+    this.infoLaunches.forEach(launch => {
+      if (launch.id == id) {
+        this.infoRocket.forEach(rocket => {
+          if (launch.rocket == rocket.id) {
+            this.rocketName = rocket.name;
+            this.rocketImage = rocket.flickr_images[1];
+          }
+        });
+      }
+    });
+  }
+
+  setLaunchId(idLanzamiento) {
     this.favoritos = [];
     let ref = this.provSer.getFavoritos();
-    ref.once("value", snapshot =>{
-      snapshot.forEach(child =>{
+    ref.once("value", snapshot => {
+      snapshot.forEach(child => {
         let value = child.val();
         this.favoritos.push(value);
-        console.log("Favoritos "+this.favoritos);
-        console.log("He encontrado "+child.val().idLanzamiento);
+        console.log("Favoritos " + this.favoritos);
+        console.log("He encontrado " + child.val().idLanzamiento);
       })
     });
-//No entra al for
-console.log(this.favoritos.length);
+    //No entra al for
+    console.log(this.favoritos.length);
     for (let i = 0; i < this.favoritos.length; i++) {
       console.log(this.favoritos[i]);
       if (this.favoritos[i].usuario == this.UsuarioIniciado && this.favoritos[i].idLanzamiento == idLanzamiento && !this.existe) {
-          this.existe = true;
-          this.meGustaIcon = true;
-         break;
-      }
-      else{
-        this.existe = false;
-        this.meGustaIcon = false;
-      }
-    }
-    if(!this.existe){
-        let ref = this._db.database.ref("Favoritos");
-        ref.push({idLanzamiento:idLanzamiento, usuario:this.UsuarioIniciado});
         this.existe = true;
         this.meGustaIcon = true;
-    }
-    else{
-        // Elimina el favorito que es igual al usuario
-        let ref2 = this.provSer.getFavoritos();
-        ref2.orderByChild("usuario").equalTo(this.UsuarioIniciado).once("value", snapshot => {
-        snapshot.forEach(child => {
-          console.log(child.key+": "+child.val());
-          console.log(child.val().idLanzamiento);
-          if (child.val().idLanzamiento == idLanzamiento && child.val().usuario == this.UsuarioIniciado) {
-              let clave = child.key;
-              ref2.child(clave).remove();
-          }
-        })
-        });
-        console.log(this.UsuarioIniciado);
+        break;
+      }
+      else {
         this.existe = false;
         this.meGustaIcon = false;
+      }
+    }
+    if (!this.existe) {
+      let ref = this._db.database.ref("Favoritos");
+      ref.push({ idLanzamiento: idLanzamiento, usuario: this.UsuarioIniciado });
+      this.existe = true;
+      this.meGustaIcon = true;
+    }
+    else {
+      // Elimina el favorito que es igual al usuario
+      let ref2 = this.provSer.getFavoritos();
+      ref2.orderByChild("usuario").equalTo(this.UsuarioIniciado).once("value", snapshot => {
+        snapshot.forEach(child => {
+          console.log(child.key + ": " + child.val());
+          console.log(child.val().idLanzamiento);
+          if (child.val().idLanzamiento == idLanzamiento && child.val().usuario == this.UsuarioIniciado) {
+            let clave = child.key;
+            ref2.child(clave).remove();
+          }
+        })
+      });
+      console.log(this.UsuarioIniciado);
+      this.existe = false;
+      this.meGustaIcon = false;
     }
   }
 }
--- src/app/lanzamientos/lanzamientos.page.ts
+++ src/app/lanzamientos/lanzamientos.page.ts
@@ -13,74 +13,75 @@ export class LanzamientosPage implements OnInit {
   infoRocket: any;
   infoLaunchpad: any;
   provSer: SpaceServiceService;
-  arrayDate:any[] = Array();
-  newArrayDate:any[] = Array();
+  arrayDate: any[] = Array();
+  newArrayDate: any[] = Array();
   constructor(private http: HTTP, public spaceService: SpaceServiceService) {
     this.provSer = spaceService
-   }
-  
+  }
+
   ngOnInit() {
-    
+
     this.http.get('https://api.spacexdata.com/v4/launches/upcoming', {}, {})
 
-    .then(res => {
-      console.log(res + "HOLA");
-      this.infoLaunches= JSON.parse(res.data);
-      console.log("ESTO");
-      for (const key in this.infoLaunches) {
-        console.log(this.infoLaunches[key].id); // Información recibida desde el server.
-        this.arrayDate.push({"id":this.infoLaunches[key].id, "date":this.infoLaunches[key].date_utc})
-      }
-      console.log(this.arrayDate);
-      
-    })
-    .catch(error => {
-      console.log(error.error); // Mensaje de error en una cadena.
-    });
+      .then(res => {
+        console.log(res + "HOLA");
+        this.infoLaunches = JSON.parse(res.data);
+        console.log("ESTO");
+        for (const key in this.infoLaunches) {
+          console.log(this.infoLaunches[key].id); // Información recibida desde el server.
+          this.arrayDate.push({ "id": this.infoLaunches[key].id, "date": this.infoLaunches[key].date_utc })
+        }
+        console.log(this.arrayDate);
+
+      })
+      .catch(error => {
+        console.log(error.error); // Mensaje de error en una cadena.
+      });
 
     this.http.get('https://api.spacexdata.com/v4/rockets', {}, {})
 
-    .then(res => {
-      console.log(res + "HOLA");
-      this.infoRocket= JSON.parse(res.data);
-      console.log(res.data); // Información recibida desde el server.
-    })
-    .catch(error => {
-      console.log(error.error); // Mensaje de error en una cadena.
-    });
+      .then(res => {
+        console.log(res + "HOLA");
+        this.infoRocket = JSON.parse(res.data);
+        console.log(res.data); // Información recibida desde el server.
+      })
+      .catch(error => {
+        console.log(error.error); // Mensaje de error en una cadena.
+      });
+      
     this.http.get('https://api.spacexdata.com/v4/launchpads', {}, {})
 
-    .then(res => {
-      console.log(res + "HOLA");
-      this.infoLaunchpad= JSON.parse(res.data);
-      console.log(res.data); // Información recibida desde el server.
-    })
-    .catch(error => {
-      console.log(error.error); // Mensaje de error en una cadena.
-    });
-    
-    
+      .then(res => {
+        console.log(res + "HOLA");
+        this.infoLaunchpad = JSON.parse(res.data);
+        console.log(res.data); // Información recibida desde el server.
+      })
+      .catch(error => {
+        console.log(error.error); // Mensaje de error en una cadena.
+      });
+
+
   }
   conutDownDate = new Date("2021-05-26T18:59:00.000Z").getTime();
-  setLaunchId(id){
+  setLaunchId(id) {
     this.provSer.setLaunchId(id);
   }
 
-  demo:any
-  x = setInterval(()=>{
-    this.newArrayDate=[]
+  demo: any
+  x = setInterval(() => {
+    this.newArrayDate = []
     for (const key in this.arrayDate) {
       var now = new Date().getTime();
-      this.conutDownDate =  new Date(this.arrayDate[key].date).getTime();
-      var distance =this.conutDownDate - now;
-      
-      var days = Math.floor(distance/(1000*60*60*24));
-      var hours = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
-        hours = hours+2;
-      var minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
-      var seconds = Math.floor((distance % (1000*60)) / 1000);
+      this.conutDownDate = new Date(this.arrayDate[key].date).getTime();
+      var distance = this.conutDownDate - now;
+
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      hours = hours + 2;
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
       this.demo = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-      this.newArrayDate.push({"id":this.arrayDate[key].id, "date":this.demo})
+      this.newArrayDate.push({ "id": this.arrayDate[key].id, "date": this.demo })
     }
   })
 }
